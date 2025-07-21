64500309c3672b8e8b697e014a93790aea84cb2b
Anasiotasio <Zxl2024903531@outlook.com>
2025-07-21 10:41:36 +0800

    反正做了题

---
+++ src/main/java/com/zxl/LeetCode/Solution/copyRandomList.java
@@ -0,0 +1,40 @@
+package com.zxl.LeetCode.Solution;
+
+import java.util.HashMap;
+
+public class copyRandomList {
+   /* HashMap<Node,Node>mp=new HashMap<>();
+    public Node copyRandomList(Node head) {
+        if(head==null){
+            return null;
+        }
+        if(!mp.containsKey(head)){
+            Node newNode=new Node(head.val);
+            mp.put(head,newNode);
+            newNode.next=copyRandomList(head.next);
+            newNode.random=copyRandomList(head.random);
+        }
+        return mp.get(head);
+    }*/
+   public Node copyRandomList(Node head) {
+      if(head==null){
+          return  null;
+      }
+      for(Node node=head;node!=null;node=node.next.next){
+          Node newNode=new Node(node.val);
+          newNode=node.next;
+          node.next=newNode.next;
+      }
+      for(Node node=head;node!=null;node=node.next.next){
+          Node newNode=node.next;
+          newNode.random=(node.random!=null)?node.random.next:null;
+      }
+      Node newHead=head.next;
+      for(Node node=head;node!=null;node=node.next){
+          Node newNode=node.next;
+          node.next=node.next.next;
+          newNode.next=(newNode.next!=null)?newNode.next.next:null;
+      }
+      return newHead;
+   }
+}
