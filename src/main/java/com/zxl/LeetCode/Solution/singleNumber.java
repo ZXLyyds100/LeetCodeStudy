067a5e7b9f7e5d5edfaac757683a6f6a87c1ef8d
Anasiotasio <Zxl2024903531@outlook.com>
2025-08-13 22:24:58 +0800

    反正做了题

---
+++ src/main/java/com/zxl/LeetCode/Solution/singleNumber.java
@@ -0,0 +1,11 @@
+package com.zxl.LeetCode.Solution;
+
+public class singleNumber {
+    public int singleNumber(int[] nums) {
+        int single = 0;
+        for(int num : nums){
+            single ^= num;
+        }
+        return single;
+    }
+}
