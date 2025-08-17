fd2c0b76f89740d51c0714a4f01e04cfc36451a9
Anasiotasio <Zxl2024903531@outlook.com>
2025-08-17 20:27:34 +0800

    完成了中等题，贪心解决canJump问题

---
+++ src/main/java/com/zxl/LeetCode/Solution/canJump.java
@@ -0,0 +1,17 @@
+package com.zxl.LeetCode.Solution;
+
+public class canJump {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int rightMost = 0;
+        for(int i = 0; i < n; i++){
+            if(i <= rightMost) {
+                rightMost = Math.max(rightMost, nums[i] + i);
+                if (rightMost >= n - 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
