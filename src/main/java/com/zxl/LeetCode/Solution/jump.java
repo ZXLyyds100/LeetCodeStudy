6bc2c7eee33900cbdadf08541205b07bc9abf2e5
Anasiotasio <Zxl2024903531@outlook.com>
2025-08-18 20:57:10 +0800

    完成了中等题，贪心解决jump，俩种解法，第一种n^2，反向贪心，第二种n

--- src/main/java/com/zxl/LeetCode/Solution/jump.java
+++ src/main/java/com/zxl/LeetCode/Solution/jump.java
@@ -4,9 +4,9 @@ public class jump {
     public int jump_1(int[] nums) {
         int pos = nums.length - 1;
         int steps = 0;
-        while(pos > 0){
-            for(int i = 0; i < pos; i++){
-                if(pos <= i + nums[i]){
+        while (pos > 0) {
+            for (int i = 0; i < pos; i++) {
+                if (pos <= i + nums[i]) {
                     steps++;
                     pos = i;
                     break;
@@ -15,18 +15,20 @@ public class jump {
         }
         return steps;
     }
-    public int jump_2(int[] nums){
-            int maxPosition = 0;
-            int end = 0;
-            int length = nums.length;
-            int steps = 0;
-            for(int i = 0; i < length - 1; i++){
-                maxPosition = Math.max(maxPosition, i + nums[i]);
-                if(i == end){
-                    end = maxPosition;
-                    steps++;
-                }
+
+    public int jump_2(int[] nums) {
+        int maxPosition = 0;
+        int end = 0;
+        int length = nums.length;
+        int steps = 0;
+        for (int i = 0; i < length - 1; i++) {
+            // 遍历每个位置能到的最远的点
+            maxPosition = Math.max(maxPosition, i + nums[i]);
+            if (i == end) {// 如果到end了的花进行更新
+                end = maxPosition;
+                steps++;
             }
-            return steps;
+        }
+        return steps;
     }
 }
