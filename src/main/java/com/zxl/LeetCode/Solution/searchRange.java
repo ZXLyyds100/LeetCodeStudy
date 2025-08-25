e1653f9bac3751997b0ac52ad1bf2aeb832e8cd5
Anasiotasio <Zxl2024903531@outlook.com>
2025-08-25 22:12:58 +0800

    完成了二分查fist和last出现

---
+++ src/main/java/com/zxl/LeetCode/Solution/searchRange.java
@@ -0,0 +1,36 @@
+package com.zxl.LeetCode.Solution;
+
+public class searchRange {
+    public int[] searchRange(int[] nums, int target) {
+        int first = -1;
+        int last = -1;
+        int left = 0, right = nums.length - 1;
+        while(left <= right){
+            int mid = (right - left) / 2 + left;
+            if(nums[mid] == target){
+                first = mid;
+                right = mid - 1;
+            }else if(nums[mid] > target){
+                right = mid - 1;
+            }else{
+                left = mid + 1;
+            }
+        }
+        left = 0;
+        right = nums.length - 1;
+        while(left <= right){
+            int mid = (right - left) / 2 + left;
+            if(nums[mid] == target){
+                last = mid;
+                left = mid + 1;
+            }else if(nums[mid] < target){
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+
+            return new int[]{first, last};
+
+    }
+}
