e24a34c0d57885a9e684dddc44b7a541947d6343
Anasiotasio <Zxl2024903531@outlook.com>
2025-09-02 10:30:36 +0800

    完成了通过使用map和优先队列实现topKFrequent

---
+++ src/main/java/com/zxl/LeetCode/Solution/topKFrequent.java
@@ -0,0 +1,38 @@
+package com.zxl.LeetCode.Solution;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class topKFrequent {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int num : nums) {
+            count.put(num, count.getOrDefault(count, 0) + 1);
+        }
+        PriorityQueue<int[]> queue = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[1] - o2[1];
+            }
+        });
+        for (Map.Entry<Integer, Integer> cnt : count.entrySet()) {
+            int num = cnt.getKey();
+            int cnt1 = cnt.getValue();
+            if (queue.size() == k) {
+                if (queue.peek()[1] < cnt1) {
+                    queue.poll();
+                    queue.offer(new int[]{num, cnt1});
+                }
+            } else {
+                queue.offer(new int[]{num, cnt1});
+            }
+        }
+        int[] res = new int[k];
+        for (int i = 0; i < k; i++) {
+            res[i] = queue.poll()[0];
+        }
+        return res;
+    }
+}
