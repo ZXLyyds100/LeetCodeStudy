97ecc218501c54034ae63c686812b30cf56491d8
Anasiotasio <Zxl2024903531@outlook.com>
2025-09-09 21:25:03 +0800

    完成了通过使用dp解决numSquares问题

---
+++ src/main/java/com/zxl/LeetCode/Solution/numSquares.java
@@ -0,0 +1,15 @@
+package com.zxl.LeetCode.Solution;
+
+public class numSquares {
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        for(int i = 1; i <= n; i++){
+            int minn = Integer.MAX_VALUE;
+            for(int j = 1; j * j <= i; j++){
+                minn = Math.min(dp[i - j * j], minn);
+            }
+            dp[i] = minn + 1;
+        }
+        return dp[n];
+    }
+}
