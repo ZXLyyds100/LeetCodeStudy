53e9ac106a10e1ad76f806ae540661be24a78fba
anastasio <Zxl2024903531@outlook.com>
2025-10-27 14:32:59 +0800

    使用中心扩展法解决最长回文子串问题，还是n方，但是比dp快很多

--- src/main/java/com/zxl/LeetCode/Solution/longestPalinrome.java
+++ src/main/java/com/zxl/LeetCode/Solution/longestPalinrome.java
@@ -4,7 +4,7 @@ import java.util.function.Consumer;
 
 public class longestPalinrome {
     //1、使用二维dp解决最长回文子串问题，这里子串在原串中必须连续
-    public String longestPalindrome(String s) {
+    /*public String longestPalindrome(String s) {
         if (s.length() < 2) {
             return s;
         }
@@ -39,5 +39,31 @@ public class longestPalinrome {
         }
         return s.substring(start, start + maxLen);
 
+    }*/
+    //2、使用中心扩展法解决，和1的时间复杂度相同
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) {
+            return "";
+        }
+        int start = 0, end = 0;
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            int len1 = expand(s, i, i);
+            int len2 = expand(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start + 1) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    public int expand(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left + 1;
     }
 }
