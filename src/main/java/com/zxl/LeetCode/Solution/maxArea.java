2f44f200c81dc7a9a17723fdb278ba51568d2677
anastasio <Zxl2024903531@outlook.com>
2025-11-04 23:46:01 +0800

    二刷0排序和水容器

--- src/main/java/com/zxl/LeetCode/Solution/maxArea.java
+++ src/main/java/com/zxl/LeetCode/Solution/maxArea.java
@@ -5,17 +5,17 @@ public class maxArea {
         System.out.println(maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}));
     }
     public static int maxArea(int[] height) {
-        int left=0,right=height.length-1;
-        int MaxArea=0;
-        while(left<right){
-            int area=Math.min(height[left],height[right])*(right-left);
-            MaxArea=Math.max(MaxArea,area);
-            if(height[left]>=height[right]){
-                right--;
-            }else{
-                left++;
-            }
-        }
-        return MaxArea;
+       int left = 0, right = height.length - 1;
+       int maxArea = 0;
+       for (int i = 0; i < height.length; i++) {
+           int area = Math.min(height[left], height[right]) * (right - left);
+           maxArea = Math.max(area, maxArea);
+           if (height[left] >= height[right]) {
+               right--;
+           } else {
+               left++;
+           }
+       }
+       return maxArea;
     }
 }
--- src/main/java/com/zxl/LeetCode/Solution/setZeros.java
+++ src/main/java/com/zxl/LeetCode/Solution/setZeros.java
@@ -2,8 +2,8 @@ package com.zxl.LeetCode.Solution;
 
 public class setZeros {
     public void setZeroes(int[][] matrix) {
-        int m=matrix.length;
-        int n=matrix[0].length;
+        int m = matrix.length;
+        int n = matrix[0].length;
         /*boolean[]row=new boolean[m];
         boolean[]col=new boolean[n];
         for(int i=0;i<m;i++){
@@ -21,41 +21,41 @@ public class setZeros {
                 }
             }
         }*/
-        boolean flagRow=false;
-        boolean flagCol=false;
-        for(int i=0;i<m;i++){
-            if(matrix[i][0]==0){
-                flagCol=true;
+        boolean flagRow = false;
+        boolean flagCol = false;
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                flagCol = true;
             }
         }
-        for(int i=0;i<n;i++){
-            if(matrix[0][i]==0){
-                flagRow=true;
+        for (int i = 0; i < n; i++) {
+            if (matrix[0][i] == 0) {
+                flagRow = true;
             }
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]==0){
-                    matrix[i][0]=0;
-                    matrix[0][j]=0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                if(matrix[i][0]==0||matrix[0][j]==0){
-                    matrix[i][j]=0;
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
                 }
             }
         }
-        if(flagRow){
-            for(int i=0;i<n;i++){
-                matrix[0][i]=0;
+        if (flagRow) {
+            for (int i = 0; i < n; i++) {
+                matrix[0][i] = 0;
             }
         }
-        if(flagCol){
-            for(int i=0;i<m;i++){
-                matrix[i][0]=0;
+        if (flagCol) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
             }
         }
     }
