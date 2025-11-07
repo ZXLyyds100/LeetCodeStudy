529b1abcb7b2a691b9b437a8f4acd229b3127c44
anastasio <Zxl2024903531@outlook.com>
2025-11-07 14:29:42 +0800

    二刷minWindow，使用滑动窗口解决s里面包含t所有字符的最小子串问题

--- src/main/java/com/zxl/LeetCode/Solution/minWindow.java
+++ src/main/java/com/zxl/LeetCode/Solution/minWindow.java
@@ -1,55 +1,55 @@
 package com.zxl.LeetCode.Solution;
+
 // s中最短的包含t中所有字符的子串
 public class minWindow {
     public String minWindow(String s, String t) {
-        int sLen=s.length();
-        int tLen=t.length();
-        if(sLen<tLen){
+        int sLen = s.length();
+        int tLen = t.length();
+        if (sLen < tLen) {
             return "";
         }
         char[] sArray = s.toCharArray();
         char[] tArray = t.toCharArray();
-        int[] winFreq=new int[128];
-        int[] tFreq=new int[128];
-        for(char c:tArray){
-            tFreq[c]++;
+        int[] winCnt = new int[128];
+        int[] tCnt = new int[128];
+        for (char c : tArray) {
+            tCnt[c]++;
         }
-        int left=0,right=0;
-        // [left,right)
-        int distance=0;
-        int begin=0;
-        int minLen=sLen+1;
-        while(right<sLen){
-            if(tFreq[sArray[right]]==0){
+        int distance = 0;
+        int left = 0, right = 0;
+        int minLen = sLen + 1;
+        int begin = 0;
+        for (int i = 0; i < sLen; i++) {
+            char c = sArray[i];
+            if (tCnt[c] == 0) {
                 right++;
                 continue;
             }
-            if(winFreq[sArray[right]]<tFreq[sArray[right]]){
+            if (winCnt[c] < tCnt[c]) {
                 distance++;
             }
-            winFreq[sArray[right]]++;
+            winCnt[c]++;
             right++;
-            while(distance==tLen){
-                if(right-left<minLen){
-                    minLen=right-left;
-                    begin=left;
+            while (distance == tLen) {
+                if (right - left < minLen) {
+                    minLen = right - left;
+                    begin = left;
                 }
-                if(tFreq[sArray[left]]==0){
+                char leftChar = sArray[left];
+                if (tCnt[leftChar] == 0) {
                     left++;
                     continue;
                 }
-                if(tFreq[sArray[left]]==winFreq[sArray[left]]){
+                if (tCnt[leftChar] == winCnt[leftChar]) {
                     distance--;
                 }
-
-                winFreq[sArray[left]]--;
                 left++;
+                winCnt[leftChar]--;
             }
         }
-        if(minLen==sLen+1){
-            return ";";
-        }else{
-            return s.substring(begin,begin+minLen);
+        if (minLen == sLen + 1) {
+            return "";
         }
+        return s.substring(begin, begin + minLen);
     }
 }
