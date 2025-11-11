795d07583bfa853b7871234a00eef5d35c84fa82
anastasio <Zxl2024903531@outlook.com>
2025-11-11 19:51:57 +0800

    二刷合并区间，就是左边界排序，但是语法不熟悉·

--- src/main/java/com/zxl/LeetCode/Solution/merge.java
+++ src/main/java/com/zxl/LeetCode/Solution/merge.java
@@ -7,22 +7,23 @@ import java.util.List;
 
 public class merge {
     public int[][] merge(int[][] intervals) {
-        if(intervals.length==0){
+        if (intervals.length == 0) {
             return new int[0][2];
         }
         Arrays.sort(intervals, new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
-                return o1[0]-o2[0];
+                return o1[0] - o2[0];
             }
         });
-        List<int[]>merged=new ArrayList<>();
-        for(int i=0;i<intervals.length;i++){
-            int L=intervals[i][0],R=intervals[i][1];
-            if(merged.size()==0||merged.get(merged.size()-1)[1]<L){
-                merged.add(new int[]{L,R});
-            }else {
-                merged.get(merged.size()-1)[1]=Math.max(merged.get(merged.size()-1)[1],R);
+        List<int[]> merged = new ArrayList<>();
+        int n = intervals.length;
+        for (int i = 0; i < n; i++) {
+            int L = intervals[i][0];
+            if (merged.size() == 0 || L > merged.get(merged.size() - 1)[1]) {
+                merged.add(new int[]{L, intervals[i][1]});
+            } else {
+                merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], intervals[i][1]);
             }
         }
         return merged.toArray(new int[merged.size()][]);
