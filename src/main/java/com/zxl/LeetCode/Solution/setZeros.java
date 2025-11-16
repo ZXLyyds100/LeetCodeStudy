bf4b7d735bba26cf93f465cf8df9d2193265194d
anastasio <Zxl2024903531@outlook.com>
2025-11-16 12:03:41 +0800

    二刷矩阵置0，让矩阵中为0的点位，行列都为0，用第一行第一列为标记数组记录是否置0，如果第一行第一列有0那么额外用俩个boolean变量记录

--- src/main/java/com/zxl/LeetCode/Solution/setZeros.java
+++ src/main/java/com/zxl/LeetCode/Solution/setZeros.java
@@ -2,39 +2,22 @@ package com.zxl.LeetCode.Solution;
 
 public class setZeros {
     public void setZeroes(int[][] matrix) {
+        boolean rowFlag = false;
+        boolean colFlag = false;
         int m = matrix.length;
         int n = matrix[0].length;
-        /*boolean[]row=new boolean[m];
-        boolean[]col=new boolean[n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]==0){
-                    row[i]=true;
-                    col[j]=true;
-                }
-            }
-        }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(row[i]||col[j]){
-                    matrix[i][j]=0;
-                }
-            }
-        }*/
-        boolean flagRow = false;
-        boolean flagCol = false;
         for (int i = 0; i < m; i++) {
             if (matrix[i][0] == 0) {
-                flagCol = true;
+                colFlag = true;
             }
         }
         for (int i = 0; i < n; i++) {
             if (matrix[0][i] == 0) {
-                flagRow = true;
+                rowFlag = true;
             }
         }
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
                 if (matrix[i][j] == 0) {
                     matrix[i][0] = 0;
                     matrix[0][j] = 0;
@@ -48,14 +31,14 @@ public class setZeros {
                 }
             }
         }
-        if (flagRow) {
-            for (int i = 0; i < n; i++) {
-                matrix[0][i] = 0;
-            }
-        }
-        if (flagCol) {
+        if (colFlag) {
             for (int i = 0; i < m; i++) {
                 matrix[i][0] = 0;
+            }
+        }
+        if (rowFlag) {
+            for (int  i = 0; i < n; i++) {
+                matrix[0][i] = 0;
             }
         }
     }
