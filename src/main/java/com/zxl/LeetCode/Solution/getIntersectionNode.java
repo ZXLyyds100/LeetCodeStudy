fe1d000ad29cac3f149bdb770e2dde283e2d9168
anastasio <Zxl2024903531@outlook.com>
2025-11-23 17:09:30 +0800

    交叉链表的交点，实际上就是循环，A走a到交叉点，走c到终点，B走b到交叉点，走c到终点，当A和B同时走a+b+c后会到交叉点

--- src/main/java/com/zxl/LeetCode/Solution/getIntersectionNode.java
+++ src/main/java/com/zxl/LeetCode/Solution/getIntersectionNode.java
@@ -4,31 +4,17 @@ package com.zxl.LeetCode.Solution;
 
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class getIntersectionNode {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        /*Set<ListNode>visited=new HashSet<>();
-        ListNode temp=headA;
-        while(temp!=null){
-            visited.add(temp);
-            temp=temp.next;
+        ListNode pa = headA;
+        ListNode pb = headB;
+        while (pa != pb) {
+            pa = pa == null ? headB : pa.next;
+            pb = pb == null ? headA : pb.next;
         }
-        temp=headB;
-        while(temp!=null){
-            if(visited.contains(temp)){
-                return temp;
-            }
-            temp=temp.next;
-        }
-        return null;*/
-
-        ListNode pA=headA;
-        ListNode pB=headB;
-        while(pA!=pB){
-            pA=pA==null?headB:pA.next;
-            pB=pB==null?headA:pB.next;
-        }
-        return pA;
+        return pa;
     }
 }
