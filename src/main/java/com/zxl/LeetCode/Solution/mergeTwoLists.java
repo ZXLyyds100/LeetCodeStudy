432cef02692290a72bf2a812cfffa2c504c4d256
anastasio <Zxl2024903531@outlook.com>
2025-11-25 21:21:33 +0800

    二刷合并俩个链表

--- src/main/java/com/zxl/LeetCode/Solution/mergeTwoLists.java
+++ src/main/java/com/zxl/LeetCode/Solution/mergeTwoLists.java
@@ -2,34 +2,27 @@ package com.zxl.LeetCode.Solution;
 
 public class mergeTwoLists {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-       /* ListNode l1=list1;
-        ListNode l2=list2;
-        if(l1==null){
-            return l2;
-        }else if(l2==null){
-            return l1;
-        }else if(l1.val<l2.val){
-            l1.next=mergeTwoLists(l1.next,l2);
-            return l1;
-        }else{
-            l2.next=mergeTwoLists(l1,l2.next);
-            return l2;
-        }*/
-        ListNode preHead=new ListNode(-1);
-        ListNode pre=preHead;
-        ListNode l1=list1;
-        ListNode l2=list2;
-        while(l1!=null&&l2!=null){
-            if(l1.val<l2.val){
-                pre.next=l1;
-                l1=l1.next;
-            }else{
-                pre.next=l2;
-                l2=l2.next;
-            }
-            pre=pre.next;
+        if (list1 == null) {
+            return list2;
         }
-        pre.next=l1==null?l2:l1;
-        return preHead.next;
+        if (list2 == null) {
+            return list1;
+        }
+        ListNode pre = new ListNode(-1);
+        ListNode dumpy = pre;
+        ListNode p1 = list1;
+        ListNode p2 = list2;
+        while (p1 != null && p2 != null) {
+            if (p1.val < p2.val) {
+                pre.next = p1;
+                p1 = p1.next;
+            } else {
+                pre.next = p2;
+                p2 = p2.next;
+            }
+            pre = pre.next;
+        }
+        pre.next = p1 == null ? p2 : p1;
+        return dumpy.next;
     }
 }
