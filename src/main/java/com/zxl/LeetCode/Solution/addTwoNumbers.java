dedde4b5cd0fbe8b8a0068e20897c221f935249c
anastasio <Zxl2024903531@outlook.com>
2025-11-25 21:53:35 +0800

    二刷链表相加

--- src/main/java/com/zxl/LeetCode/Solution/addTwoNumbers.java
+++ src/main/java/com/zxl/LeetCode/Solution/addTwoNumbers.java
@@ -2,28 +2,29 @@ package com.zxl.LeetCode.Solution;
 
 public class addTwoNumbers {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode head=null,tail=null;
-        int carry=0;
-        while(l1!=null||l2!=null){
-            int n1=l1==null?0:l1.val;
-            int n2=l2==null?0:l2.val;
-            int sum=carry+n1+n2;
-            if(head==null){
-                head=tail=new ListNode(sum%10);
-            }else{
-                tail.next=new ListNode(sum%10);
-                tail=tail.next;
+        ListNode head = null, tail = null;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int n1 = l1 == null ? 0 : l1.val;
+            int n2 = l2 == null ? 0 : l2.val;
+            int sum = n1 + n2 + carry;
+            if (head == null) {
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
             }
-            carry=sum/10;
-            if(l1!=null){
-                l1=l1.next;
+
+            carry = sum / 10;
+            if (l1 != null) {
+                l1 = l1.next;
             }
-            if(l2!=null){
-                l2=l2.next;
+            if (l2 != null) {
+                l2 = l2.next;
             }
         }
-        if (carry>0) {
-            tail.next=new ListNode(carry);
+        if (carry > 0) {
+            tail.next = new ListNode(carry);
         }
         return head;
     }
