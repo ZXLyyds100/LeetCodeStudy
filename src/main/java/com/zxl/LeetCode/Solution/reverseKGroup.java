62b1e8b2f69a71b8d6b08967f906e91d006f924a
anastasio <Zxl2024903531@outlook.com>
2025-12-02 00:29:45 +0800

    二刷k个一组反转链表

--- src/main/java/com/zxl/LeetCode/Solution/reverseKGroup.java
+++ src/main/java/com/zxl/LeetCode/Solution/reverseKGroup.java
@@ -3,38 +3,40 @@ package com.zxl.LeetCode.Solution;
 
 public class reverseKGroup {
     public ListNode reverseKGroup(ListNode head, int k) {
-        ListNode hair=new ListNode(0);
-        hair.next=head;
-        ListNode pre=hair;
-        while(head!=null){
-            ListNode tail=pre;
-            for(int i=0;i<k;i++){
-                tail=tail.next;
-                if(tail==null){
+        ListNode hair = new ListNode(0);
+        hair.next = head;
+        ListNode prev = hair;
+        while (head != null) {
+            ListNode tail = prev;
+            for (int i = 0; i < k; i++) {
+                tail = tail.next;
+                if (tail == null) {
                     return hair.next;
                 }
             }
-            ListNode nex=tail.next;
-            ListNode[] reverse = reverse(head, tail);
-            head=reverse[0];
-            tail=reverse[1];
-            pre.next=head;
-            tail.next=nex;
-            pre=tail;
-            head=tail.next;
+            ListNode nex = tail.next;
+            ListNode[] tuple = reverse(head, tail);
+            head = tuple[0];
+            tail = tuple[1];
+            prev.next = head;
+            tail.next = nex;
+            head = tail.next;
+            prev = tail;
         }
         return hair.next;
     }
-    public ListNode[] reverse(ListNode head,ListNode tail){
-        ListNode prev=tail.next;
-        ListNode cur=head;
-        while(prev!=tail){
-            ListNode temp=cur.next;
-            cur.next=prev;
-            prev=cur;
-            cur=temp;
+
+    public ListNode[] reverse(ListNode head, ListNode tail) {
+        ListNode prev = tail.next;
+        ListNode cur = head;
+        while (prev != tail) {
+            ListNode nex = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = nex;
         }
-        return new ListNode[]{tail,head};
+        return new ListNode[]{tail, head};
+
     }
 
     public static void main(String[] args) {
