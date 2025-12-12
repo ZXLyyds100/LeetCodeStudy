bbdad37dc21f693d4a62b6d3eda6fe60f2d3164a
anastasio <Zxl2024903531@outlook.com>
2025-12-13 00:47:23 +0800

    二刷对称二叉树

--- src/main/java/com/zxl/LeetCode/Solution/isSymmetric.java
+++ src/main/java/com/zxl/LeetCode/Solution/isSymmetric.java
@@ -5,12 +5,12 @@ public class isSymmetric {
         return check(root.left,root.right);
     }
     public boolean check(TreeNode left,TreeNode right){
-        if(right==null&&left==null){
+        if (left == null && right == null) {
             return true;
         }
-        if(right==null||left==null){
+        if (left == null || right == null) {
             return false;
         }
-        return right.val==left.val&&check(left.left,right.right)&&check(left.right,right.left);
+        return left.val == right.val && check(left.left, right.right) && check(left.right, right.left);
     }
 }
