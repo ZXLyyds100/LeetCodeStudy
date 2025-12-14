59a673d2ac78f5af2b54e04f2f977b1d19fbd32c
anastasio <Zxl2024903531@outlook.com>
2025-12-14 19:26:32 +0800

    二刷判断是否合法BST

--- src/main/java/com/zxl/LeetCode/Solution/isValidBST.java
+++ src/main/java/com/zxl/LeetCode/Solution/isValidBST.java
@@ -2,15 +2,15 @@ package com.zxl.LeetCode.Solution;
 
 public class isValidBST {
     public boolean isValidBST(TreeNode root) {
-        return solve(root,Long.MIN_VALUE,Long.MIN_VALUE);
+        return solve(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
     public boolean solve(TreeNode root,long lower,long upper){
-        if(root==null){
+        if (root == null) {
             return true;
         }
-        if(root.val<=lower||root.val>=upper){
+        if (root.val <= lower || root.val >= upper) {
             return false;
         }
-        return solve(root.left,lower,root.val)&&solve(root.right,root.val,upper);
+        return solve(root.left, lower, root.val) && solve(root.right, root.val, upper);
     }
 }
