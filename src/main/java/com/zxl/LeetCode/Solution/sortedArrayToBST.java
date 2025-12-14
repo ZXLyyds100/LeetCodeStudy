033e5855d69fddb596907e16111dbe0ea9d3e3a9
anastasio <Zxl2024903531@outlook.com>
2025-12-14 19:16:08 +0800

    二刷有序数组转为BST

--- src/main/java/com/zxl/LeetCode/Solution/sortedArrayToBST.java
+++ src/main/java/com/zxl/LeetCode/Solution/sortedArrayToBST.java
@@ -6,13 +6,13 @@ public class sortedArrayToBST {
         return solve(nums,0,nums.length-1);
     }
     public TreeNode solve(int[] nums,int left,int right){
-        if(left>right){
+        if (left > right) {
             return null;
         }
-        int mid=(left+right+1)/2;
-        TreeNode node=new TreeNode(nums[mid]);
-        node.left=solve(nums,left,mid-1);
-        node.right=solve(nums,mid+1,right);
+        int mid =(left + right) >> 1;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = solve(nums, left, mid - 1);
+        node.right = solve(nums, mid + 1, right);
         return node;
     }
 }
