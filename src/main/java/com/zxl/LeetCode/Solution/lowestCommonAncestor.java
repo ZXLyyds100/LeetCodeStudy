2b6eeadac932b99142be665622ffaa82951dad19
anastasio <Zxl2024903531@outlook.com>
2025-12-19 21:00:35 +0800

    二刷最近公共祖先

--- src/main/java/com/zxl/LeetCode/Solution/lowestCommonAncestor.java
+++ src/main/java/com/zxl/LeetCode/Solution/lowestCommonAncestor.java
@@ -1,22 +1,26 @@
 package com.zxl.LeetCode.Solution;
 
 public class lowestCommonAncestor {
-   /* TreeNode ans;
+    TreeNode ans;
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null){
+        if (root == null) {
             return null;
         }
         dfs(root, p, q);
         return ans;
     }
-    public boolean dfs(TreeNode root, TreeNode p, TreeNode q){
-        if(root == null)return false;
-        boolean l = dfs(root.left, p, q);
-        boolean r = dfs(root.right, p, q);
-        if((l && r)||((root.val == p.val)||(root.val == q.val) && (l || r))){
+    public boolean dfs (TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return false;
+        }
+        boolean isCurrentNode = (root == p || root == q);
+        boolean leftContains = dfs(root.left, p, q);
+        boolean rightContains = dfs(root.right, p, q);
+        if ((leftContains && rightContains) || (isCurrentNode && (rightContains || leftContains))) {
             ans = root;
         }
-        return l || r || ((root.val == p.val) || (root.val == q.val));
-    }*/
+        return leftContains || rightContains || isCurrentNode;
+    }
+
 
 }
