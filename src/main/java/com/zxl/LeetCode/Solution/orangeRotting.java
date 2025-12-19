b8d49cb853c2951979051228d32dea91555e107d
anastasio <Zxl2024903531@outlook.com>
2025-12-19 21:52:33 +0800

    二刷橘子

--- src/main/java/com/zxl/LeetCode/Solution/orangeRotting.java
+++ src/main/java/com/zxl/LeetCode/Solution/orangeRotting.java
@@ -7,42 +7,45 @@ import java.util.Queue;
 
 public class orangeRotting {
     public int orangesRotting(int[][] grid) {
-        if(grid == null || grid[0].length == 0){
+        if (grid == null) {
             return 0;
         }
-        int[] dr = new int[]{0, 1, 0, -1};
-        int[] dc = new int[]{1, 0, -1, 0};
         int nr = grid.length;
         int nc = grid[0].length;
         Queue<Integer> q = new LinkedList<>();
         Map<Integer, Integer> time = new HashMap<>();
-        for(int i = 0; i < nr; i++){
-            for(int j = 0; j < nc; j++){
-                if(grid[i][j] == 2){
-                    q.add(i * nc + j);
-                    time.put(i * nc + j, 0);
+        int[] dr = new int[]{1, -1, 0, 0};
+        int[] dc = new int[]{0, 0, 1, -1};
+        for (int i = 0; i < nr; i++) {
+            for (int j = 0; j < nc; j++) {
+                if (grid[i][j] == 2) {
+                    int code = i * nc + j;
+                    q.add(code);
+                    time.put(code, 0);
                 }
             }
         }
         int ans = 0;
-        while(!q.isEmpty()){
+        while (!q.isEmpty()) {
             int code = q.remove();
             int r = code / nc;
             int c = code % nc;
-            for(int k = 0; k < 4; k++){
-                int r1 = r + dr[k];
-                int c1 = c + dc[k];
-                if(r1 >= 0 && c1 >= 0 && r1 < nr && c1 < nc && grid[r1][c1] == 1){
+            for (int i = 0; i < 4; i++) {
+                int r1 = r + dr[i];
+                int c1 = c + dc[i];
+                if (r1 >= 0 && c1 >= 0 && r1 < nr && c1 < nc && grid[r1][c1] == 1) {
                     grid[r1][c1] = 2;
-                    q.add(r1 * nc + c1);
-                    time.put(r1 * nc + c1, time.get(r * nc + c) + 1);
-                    ans =  time.get(r1 * nc + c1);
+                    int newCode = r1 * nc + c1;
+                    q.add(newCode);
+                    int newTime = time.get(code) + 1;
+                    time.put(newCode, newTime);
+                    ans = newTime;
                 }
             }
         }
-        for(int[] row : grid){
-            for(int e : row){
-                if(e == 1){
+        for (int[] e : grid) {
+            for (int a : e) {
+                if (a == 1) {
                     return -1;
                 }
             }
