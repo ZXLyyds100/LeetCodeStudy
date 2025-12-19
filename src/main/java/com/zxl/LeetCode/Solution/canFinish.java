43204a67cf092fafa90777a3f7dcb39f5509608f
anastasio <Zxl2024903531@outlook.com>
2025-12-19 22:49:40 +0800

    二刷课表

--- src/main/java/com/zxl/LeetCode/Solution/canFinish.java
+++ src/main/java/com/zxl/LeetCode/Solution/canFinish.java
@@ -5,27 +5,28 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class canFinish {
-    int[] d;
     int[] e;
+    int[] d;
     int[] ne;
     int[] h;
     int idx = 0;
-    int n;
-    public void add(int a, int b){
+    public void add(int a, int b) {
         e[idx] = b;
         ne[idx] = h[a];
         h[a] = idx++;
     }
+    int n, m;
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int m = prerequisites.length;
         n = numCourses;
-        d = new int[n];
-        h = new int[n];
+        m = prerequisites.length;
         e = new int[m];
         ne = new int[m];
+        h = new int[n];
+        d = new int[n];
         Arrays.fill(h, -1);
-        for(int[] edge : prerequisites){
-            int a = edge[0], b = edge[1];
+        for (int[] edge : prerequisites) {
+            int a = edge[0];
+            int b = edge[1];
             add(b, a);
             d[a]++;
         }
@@ -34,20 +35,20 @@ public class canFinish {
     public boolean topsort(){
         Queue<Integer> q = new LinkedList<>();
         int cnt = 0;
-        for(int i = 0; i < n; i++){
-            if(d[i] == 0){
-                cnt++;
+        for (int i = 0; i < n; i++) {
+            if (d[i] == 0) {
                 q.offer(i);
+                cnt++;
             }
         }
-        while(!q.isEmpty()){
-            int t = q.poll();
-            for(int i = h[t]; i != -1; i = ne[i]){
+        while (!q.isEmpty()) {
+            int k = q.poll();
+            for (int i = h[k]; i != -1; i = ne[i]) {
                 int j = e[i];
                 d[j]--;
-                if(d[j] == 0){
-                    q.offer(j);
+                if (d[j] == 0) {
                     cnt++;
+                    q.offer(j);
                 }
             }
         }
