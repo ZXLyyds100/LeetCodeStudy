c7c90e22d18f6bb5b2b8f406493693fec46efb3e
anastasio <Zxl2024903531@outlook.com>
2025-12-19 20:39:54 +0800

    二刷路径和

--- src/main/java/com/zxl/LeetCode/Solution/pathSum.java
+++ src/main/java/com/zxl/LeetCode/Solution/pathSum.java
@@ -1,46 +1,30 @@
 package com.zxl.LeetCode.Solution;
 
+import com.sun.source.tree.Tree;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class pathSum {
-    /*public int pathSum(TreeNode root, int targetSum) {
-        if(root == null)return 0;
-        int ret = rootSum(root, targetSum);
-        ret += pathSum(root.left, targetSum);z
-        ret += pathSum(root.right, targetSum);
-        return ret;
-    }
-    public int rootSum(TreeNode root, int targetSum){
-        if(root == null){
-            return 0;
-        }
-        int ret = 0;
-        int val = root.val;
-        if(val == targetSum){
-            ret++;
-        }
-        ret += rootSum(root.left, targetSum-val);
-        ret += rootSum(root.right, targetSum-val);
-        return ret;
-    }*/
+
     public int pathSum(TreeNode root, int targetSum) {
-        Map<Long, Integer>mp = new HashMap<>();
-        mp.put(0L, 1);
-        return dfs(root, mp, 0, targetSum);
+        Map<Long, Integer> prefix = new HashMap<>();
+        prefix.put(0L, 1);
+        return dfs(prefix, 0L, targetSum, root);
     }
-    public int dfs(TreeNode root, Map<Long, Integer>prefix, long curr, int targetSum){
-        if(root == null){
+    public int dfs(Map<Long, Integer> prefix, Long curr, int targetSum, TreeNode root) {
+        if (root == null) {
             return 0;
         }
         int ret = 0;
-        curr += root.val;// curr + A = targetSum，看集合里面有没有
+        curr += root.val;
         ret = prefix.getOrDefault(curr - targetSum, 0);
         prefix.put(curr, prefix.getOrDefault(curr, 0) + 1);
-        ret += dfs(root.left, prefix, curr, targetSum);
-        ret += dfs(root.right, prefix, curr, targetSum);
+        ret += dfs(prefix, curr, targetSum, root.left);
+        ret += dfs(prefix, curr, targetSum, root.right);
         prefix.put(curr, prefix.get(curr) - 1);
         return ret;
     }
+
 
 }
