6fda1199a3a30115b5ec6e431a73144460729cf1
anastasio <Zxl2024903531@outlook.com>
2025-12-25 14:32:50 +0800

    二刷电话号码

--- src/main/java/com/zxl/LeetCode/Solution/letterCombinations.java
+++ src/main/java/com/zxl/LeetCode/Solution/letterCombinations.java
@@ -7,34 +7,36 @@ import java.util.Map;
 
 public class letterCombinations {
     public List<String> letterCombinations(String digits) {
-        List<String> ans = new ArrayList<>();
-        if(digits.length() == 0){
-            return ans;
+        if (digits == null) {
+            return new ArrayList<>();
         }
-        Map<Character, String> phone = new HashMap<>(){{
-            put('2', "abc");
-            put('3', "def");
-            put('4', "ghi");
-            put('5', "jkl");
-            put('6', "mno");
-            put('7', "pqrs");
-            put('8', "tuv");
-            put('9', "wxyz");
-        }};
-        backtrace(ans, new StringBuffer(), 0, digits, phone);
+        Map<Character, String> phone = new HashMap<>(){
+            {
+                put('2', "abc");
+                put('3', "def");
+                put('4', "ghi");
+                put('5', "jkl");
+                put('6', "mno");
+                put('7', "pqrs");
+                put('8', "tuv");
+                put('9', "wxyz");
+            }
+        };
+        List<String> ans = new ArrayList<>();
+        dfs(new StringBuffer(), 0, phone, digits, ans);
         return ans;
     }
-    public void backtrace (List<String> ans, StringBuffer item, int index, String digits, Map<Character, String> phone){
-        if(item.length() == digits.length()){
-            ans.add(item.toString());
-        }else{
-            char digit = digits.charAt(index);
-            String letter = phone.get(digit);
-            for(int i = 0; i < letter.length(); i++){
-                item.append(letter.charAt(i));
-                backtrace(ans, item, index + 1, digits, phone);
-                item.deleteCharAt(index);
-            }
+    private void dfs(StringBuffer item, int index, Map<Character, String> phone, String digits, List<String> ans) {
+        if (item.length() == digits.length()) {
+            ans.add(new String(item));
+            return;
+        }
+        Character digit = digits.charAt(index);
+        String letters = phone.get(digit);
+        for (int i = 0; i < letters.length(); i++) {
+            item.append(letters.charAt(i));
+            dfs(item, index + 1, phone, digits, ans);
+            item.deleteCharAt(item.length() - 1);
         }
     }
 }
