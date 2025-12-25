0d34be55d805fa455ea1da4b4fd191bb99e0d439
anastasio <Zxl2024903531@outlook.com>
2025-12-25 14:56:09 +0800

    二刷有重复数字的组合

--- src/main/java/com/zxl/LeetCode/Solution/combinationSum.java
+++ src/main/java/com/zxl/LeetCode/Solution/combinationSum.java
@@ -5,21 +5,24 @@ import java.util.List;
 
 public class combinationSum {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        if (candidates == null) {
+            return new ArrayList<>();
+        }
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> item = new ArrayList<>();
-        dfs(ans, item, 0, 0, target, candidates);
+        dfs(ans, item, 0, target, 0, candidates);
         return ans;
     }
-    public void dfs(List<List<Integer>> ans, List<Integer> item, int index, int sum, int target, int[] candidates){
-        if(sum >= target){
-            if(sum == target){
+    private void dfs(List<List<Integer>> ans, List<Integer> item, int index, int target, int sum, int[] candidates) {
+        if (sum >= target) {
+            if (sum == target) {
                 ans.add(new ArrayList<>(item));
             }
             return;
         }
-        for(int i = index; i < candidates.length; i++){
+        for (int i = index; i < candidates.length; i++) {
             item.add(candidates[i]);
-            dfs(ans, item, i, sum + candidates[i], target, candidates);
+            dfs(ans, item, i, target, sum + candidates[i], candidates);
             item.remove(item.size() - 1);
         }
     }
