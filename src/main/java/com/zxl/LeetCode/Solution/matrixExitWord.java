bcf9c4afa3ee1982ea40962caff9999e3db93a82
anastasio <Zxl2024903531@outlook.com>
2025-12-27 22:54:25 +0800

    二刷dfs寻找单词，其实就是dfs加四个方向搜索

--- src/main/java/com/zxl/LeetCode/Solution/matrixExitWord.java
+++ src/main/java/com/zxl/LeetCode/Solution/matrixExitWord.java
@@ -1,13 +1,19 @@
 package com.zxl.LeetCode.Solution;
 
 public class matrixExitWord {
+    int n;
+    int m;
+    boolean[][] visited;
+
     public boolean exist(char[][] board, String word) {
-        int n = board.length, m = board[0].length;
-        boolean[][] visited = new boolean[n][m];
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(!visited[i][j]){
-                    if(dfs(visited, i, j, 0, board, word)){
+
+        n = board.length;
+        m = board[0].length;
+        visited = new boolean[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!visited[i][j]) {
+                    if (dfs(word, 0, i, j,  board)) {
                         return true;
                     }
                 }
@@ -15,32 +21,28 @@ public class matrixExitWord {
         }
         return false;
     }
-    public boolean dfs(boolean[][] visited, int x, int y, int k, char[][] board, String word){
-        if(board[x][y] != word.charAt(k)){
+    public boolean dfs(String word, int k, int x, int y, char[][] board) {
+        if (board[x][y] != word.charAt(k)) {
             return false;
         }
-        if(k == word.length() - 1){
+        if (k == word.length() - 1) {
             return true;
         }
         visited[x][y] = true;
-        boolean result = false;
         int[] dx = new int[]{1, -1, 0, 0};
         int[] dy = new int[]{0, 0, 1, -1};
-        for(int i = 0; i < 4; i++){
+        boolean result = false;
+        for (int i = 0; i < 4; i++) {
             int x1 = x + dx[i];
             int y1 = y + dy[i];
-            if(x1 >=0 && y1 >= 0 && x1 < board.length && y1 < board[0].length){
-                if(!visited[x1][y1]){
-                    boolean flag = dfs(visited, x1, y1, k + 1, board, word);
-                    if(flag){
-                        result = true;
-                        break;
-                    }
+            if (x1 >= 0 && y1 >= 0 && x1 < n && y1 < m && !visited[x1][y1]) {
+                if (dfs(word, k + 1, x1, y1, board)) {
+                    result = true;
+                    break;
                 }
             }
         }
         visited[x][y] = false;
         return result;
     }
-
 }
