b9b9a7ad834c9adb914b1fac6a763c23adc5bd33
anastasio <Zxl2024903531@outlook.com>
2025-12-28 17:20:18 +0800

    二刷二分求插入位置

--- src/main/java/com/zxl/LeetCode/Solution/searchInsert.java
+++ src/main/java/com/zxl/LeetCode/Solution/searchInsert.java
@@ -1,19 +1,21 @@
 package com.zxl.LeetCode.Solution;
 
+
 public class searchInsert {
     public int searchInsert(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
-        while(left < right){
+        while (left < right) {
             int mid = left + (right - left) / 2;
-            if(nums[mid] == target){
+            if (nums[mid] == target) {
                 return mid;
-            }else if(nums[mid] < target){
+            } else if(nums[mid] < target) {
                 left = mid + 1;
-            }else {
+            } else if(nums[mid] > target) {
                 right = mid - 1;
             }
         }
+        // 最终的left == right是第一个>=target的index，如果全都<target，那就返回left + 1
         return nums[left] < target ? left + 1 : left;
     }
 }
