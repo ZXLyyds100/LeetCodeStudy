378ea30150eb8e48e90c8d2c497c1d385e7247b2
anastasio <Zxl2024903531@outlook.com>
2025-12-28 18:55:38 +0800

    二刷二分Range，并且总结了俩中模板，左闭右开和左闭右闭的写法

--- src/main/java/com/zxl/LeetCode/Solution/searchInsert.java
+++ src/main/java/com/zxl/LeetCode/Solution/searchInsert.java
@@ -3,19 +3,46 @@ package com.zxl.LeetCode.Solution;
 
 public class searchInsert {
     public int searchInsert(int[] nums, int target) {
+        // 俩中模板，一种左闭右闭区间[l,r]，也就是现在的写法
+        // left必须<=right,然后left = mid + 1，right = mid - 1
+        // 循环结束时left时第一个>target的值
+        // right是第一个<target的值
+        // 此时left > right
+        //left=0, right=size-1
+        //判断是否存在必须在循环中判断,也就是nums[mid]==target
         int left = 0;
         int right = nums.length - 1;
-        while (left < right) {
+        while (left <= right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
                 return mid;
-            } else if(nums[mid] < target) {
-                left = mid + 1;
-            } else if(nums[mid] > target) {
+            } else if (nums[mid] > target) {
                 right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
-        // 最终的left == right是第一个>=target的index，如果全都<target，那就返回left + 1
-        return nums[left] < target ? left + 1 : left;
+        return left;
+    }
+    public int searchInsert2(int[] nums, int target) {
+        // 这种是左闭右开区间，left < right,right=size
+        // 循环结束后的left == right，并且都是第一个>或者>=target的值，判断是否存在需要在循环外判断
+        // left要小于size并且nums[left]==target才存在
+        // 并且要记住[left,right)
+        // 当nums[mid]<target的时候left=mid+1,此时区间不会出问题
+        // 如果nums[mid]<=target的时候left=mid + 1,区间就出问题了，因为是左边是闭合的所以如果
+        // 恰好nums[left]==target的话就不在这区间了
+        // 对于第一个if判断，如果nums[left]<target，left=mid+1，否者right=mid，那么就是找第一个>或者>=target的值
+        // 如果是nums[left]<=target那就是第一个大于target的值
+        int left = 0, right = nums.length;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
     }
 }
--- src/main/java/com/zxl/LeetCode/Solution/searchRange.java
+++ src/main/java/com/zxl/LeetCode/Solution/searchRange.java
@@ -5,32 +5,72 @@ public class searchRange {
         int first = -1;
         int last = -1;
         int left = 0, right = nums.length - 1;
-        while(left <= right){
-            int mid = (right - left) / 2 + left;
-            if(nums[mid] == target){
+        // 这就是左闭右闭区间，循环中能找到，然后用right=mid-1和left=mid+1进行划分
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
                 first = mid;
                 right = mid - 1;
-            }else if(nums[mid] > target){
-                right = mid - 1;
-            }else{
+            } else if (nums[mid] < target) {
                 left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
         left = 0;
         right = nums.length - 1;
-        while(left <= right){
-            int mid = (right - left) / 2 + left;
-            if(nums[mid] == target){
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
                 last = mid;
                 left = mid + 1;
-            }else if(nums[mid] < target){
-                left = mid + 1;
-            }else{
+            } else if (nums[mid] > target) {
                 right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
-
-            return new int[]{first, last};
+        return new int[]{first, last};
 
     }
+    // 这个就是第二种写法，左闭右开的
+    class Solution {
+        public int[] searchRange(int[] nums, int target) {
+            int n = nums.length;
+            if (n == 0) return new int[]{-1, -1};
+
+            // 找 first：第一个 >= target
+            int left = 0, right = n;
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] < target) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+
+            // 如果不存在 target
+            if (left == n || nums[left] != target) {
+                return new int[]{-1, -1};
+            }
+            int first = left;
+
+            // 找 last：第一个 > target
+            left = 0;
+            right = n;
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] <= target) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+            int last = left - 1;
+
+            return new int[]{first, last};
+        }
+    }
+
 }
