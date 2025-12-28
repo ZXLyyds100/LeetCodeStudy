e0c24e77b29c5ca9c08a4d0acfafcff394b13f71
anastasio <Zxl2024903531@outlook.com>
2025-12-28 15:37:30 +0800

    二刷n皇后

--- src/main/java/com/zxl/LeetCode/Solution/solveNQueens.java
+++ src/main/java/com/zxl/LeetCode/Solution/solveNQueens.java
@@ -5,28 +5,32 @@ import java.util.Arrays;
 import java.util.List;
 
 public class solveNQueens {
+    boolean[] col;
+    boolean[] udg;
+    boolean[] dg;
+    char[][] board;
     public List<List<String>> solveNQueens(int n) {
         if (n == 0) {
             return new ArrayList<>();
         }
-        char[][] board = new char[n][n];
+        col = new boolean[n];
+        dg = new boolean[2 * n - 1];
+        udg = new boolean[2 * n - 1];
+        board = new char[n][n];
         for (int i = 0; i < n; i++) {
             Arrays.fill(board[i], '.');
         }
-        List<List<String>> result = new ArrayList<>();
-        boolean[] col = new boolean[n];
-        boolean[] dg = new boolean[2 * n - 1];
-        boolean[] udg = new boolean[2 * n - 1];
-        dfs(0, n, board, result, col, dg, udg);
-        return result;
+        List<List<String>> ans = new ArrayList<>();
+        dfs(0, n, ans);
+        return ans;
     }
-    public void dfs (int depth, int n, char[][] board, List<List<String>> result, boolean[] col, boolean[] dg, boolean[] udg) {
+    public void dfs(int depth, int n, List<List<String>> ans) {
         if (depth == n) {
             List<String> item = new ArrayList<>();
             for (int i = 0; i < n; i++) {
                 item.add(new String(board[i]));
             }
-            result.add(item);
+            ans.add(item);
             return;
         }
         for (int i = 0; i < n; i++) {
@@ -35,12 +39,13 @@ public class solveNQueens {
                 dg[depth + i] = true;
                 udg[depth - i + n - 1] = true;
                 board[depth][i] = 'Q';
-                dfs(depth + 1, n, board, result, col, dg, udg);
-                board[depth][i] = '.';
+                dfs(depth + 1, n , ans);
                 col[i] = false;
-                dg[depth + i] = false;
+                dg[i + depth] = false;
                 udg[depth - i + n - 1] = false;
+                board[depth][i] = '.';
             }
         }
     }
+
 }
