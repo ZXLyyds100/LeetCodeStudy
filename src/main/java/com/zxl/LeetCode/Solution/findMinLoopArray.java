ceedcb10c23a13c93f0ec7008ac9c6890a963c79
anastasio <Zxl2024903531@outlook.com>
2025-12-30 01:51:35 +0800

    二刷旋转数组求最小，实际上就是特殊的二分，看什么时候会到正确的区间

--- src/main/java/com/zxl/LeetCode/Solution/findMinLoopArray.java
+++ src/main/java/com/zxl/LeetCode/Solution/findMinLoopArray.java
@@ -3,13 +3,13 @@ package com.zxl.LeetCode.Solution;
 public class findMinLoopArray {
     public int findMin(int[] nums) {
         int n = nums.length;
-        int l = 0, r = n - 1;
-        while(l < r){
+        int l = 0, r = n;
+        while (l < r) {
             int mid = (r - l) / 2 + l;
-            if(nums[mid] < nums[l]){
-                r = mid;
-            }else{
+            if (nums[mid] > nums[r]) {
                 l = mid + 1;
+            } else {
+                r = mid;
             }
         }
         return nums[l];
