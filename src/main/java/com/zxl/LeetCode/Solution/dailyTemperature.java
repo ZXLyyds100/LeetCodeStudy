23ef17c2bb660471f32564080049e45cc574af60
anastasio <Zxl2024903531@outlook.com>
2026-01-28 20:15:35 +0800

    shuati

--- src/main/java/com/zxl/LeetCode/Solution/dailyTemperature.java
+++ src/main/java/com/zxl/LeetCode/Solution/dailyTemperature.java
@@ -6,15 +6,15 @@ import java.util.LinkedList;
 public class dailyTemperature {
     public int[] dailyTemperatures(int[] temperatures) {
         int n = temperatures.length;
+        Deque<Integer> stack = new LinkedList<>();
         int[] ans = new int[n];
-        Deque<Integer> stk = new LinkedList<>();
         for (int i = 0; i < n; i++) {
             int now = temperatures[i];
-            while(!stk.isEmpty() && now > temperatures[stk.peek()]){
-                int prevIndex = stk.pop();
+            while (!stack.isEmpty() && now > temperatures[stack.peek()]) {
+                int prevIndex = stack.pop();
                 ans[prevIndex] = i - prevIndex;
             }
-            stk.push(i);
+            stack.push(i);
         }
         return ans;
     }
--- src/main/java/com/zxl/LeetCode/Solution/findKthLargest.java
+++ src/main/java/com/zxl/LeetCode/Solution/findKthLargest.java
@@ -8,23 +8,21 @@ class findKthLargest {
 
     public void quick_sort(int[] nums, int l, int r) {
         if (l >= r) return;
-
         int i = l, j = r;
-        int pivot = nums[l + (r - l) / 2]; // 中点作为基准
-
+        int x = nums[l + (r - l) / 2];
         while (i <= j) {
-            while (nums[i] < pivot) i++;
-            while (nums[j] > pivot) j--;
-            if (i <= j) {
+            while (nums[i] < x) i++;
+            while (nums[j] > x) j--;
+            if (i < j) {
                 int t = nums[i];
                 nums[i] = nums[j];
                 nums[j] = t;
-                i++;
                 j--;
+                i++;
             }
-        }
 
-        if (l < j) quick_sort(nums, l, j);
-        if (i < r) quick_sort(nums, i, r);
+        }
+        quick_sort(nums, l, j);
+        quick_sort(nums, j + 1, r);
     }
 }
