db46d25b5bb9e1114c7ea7fd9eeb973558b4bb35
anastasio <Zxl2024903531@outlook.com>
2026-02-02 19:07:02 +0800

    对于每个j，只需要知道左边最小的ai就可以了，那么只需要一次遍历的时候顺便记录左边最小的ai即可

--- src/main/java/com/zxl/LeetCode/Solution/maxProfit.java
+++ src/main/java/com/zxl/LeetCode/Solution/maxProfit.java
@@ -2,14 +2,12 @@ package com.zxl.LeetCode.Solution;
 
 public class maxProfit {
     public int maxProfit(int[] prices) {
-        int minPrice = Integer.MAX_VALUE;
+        int minPrice = prices[0];
         int maxProfit = 0;
-        for(int i = 0; i < prices.length; i++){
-            if(minPrice > prices[i]){
-                minPrice = prices[i];
-            }else if(prices[i] - minPrice > maxProfit){
-                maxProfit = prices[i] - minPrice;
-            }
+
+        for (int i = 1; i < prices.length; i++) {
+            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+            minPrice = Math.min(prices[i], minPrice);
         }
         return maxProfit;
     }
