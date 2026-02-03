2630bc3a7c2ebecb5a866c35a452552c1005ebdc
anastasio <Zxl2024903531@outlook.com>
2026-02-03 19:47:28 +0800

    维护每个字母能到的最右下标，然后在遍历的时候，如果当前的i==我们的当前已遍历的字母的最大覆盖范围，那就分割

--- src/main/java/com/zxl/LeetCode/Solution/partitionLabels.java
+++ src/main/java/com/zxl/LeetCode/Solution/partitionLabels.java
@@ -6,18 +6,17 @@ import java.util.List;
 public class partitionLabels {
     public List<Integer> partitionLabels(String s) {
         int[] last = new int[26];
-        int start = 0;
         int end = 0;
-        int n = s.length();
-        for (int i = 0; i < n; i++) {
+        int start = 0;
+        for (int i = 0; i < s.length(); i++) {
             last[s.charAt(i) - 'a'] = i;
         }
         List<Integer> ans = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < s.length(); i++) {
             end = Math.max(end, last[s.charAt(i) - 'a']);
             if (i == end) {
-                ans.add(end - start + 1);
-                start = i + 1;
+                ans.add(end - start);
+                start = end;
             }
         }
         return ans;
