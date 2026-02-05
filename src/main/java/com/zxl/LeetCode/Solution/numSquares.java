a3ceb0467bbd0ebc8b25836660f1f1b985db6323
anastasio <Zxl2024903531@outlook.com>
2026-02-05 22:52:54 +0800

    shuati

--- src/main/java/com/zxl/LeetCode/Solution/numSquares.java
+++ src/main/java/com/zxl/LeetCode/Solution/numSquares.java
@@ -3,10 +3,10 @@ package com.zxl.LeetCode.Solution;
 public class numSquares {
     public int numSquares(int n) {
         int[] dp = new int[n + 1];
-        for(int i = 1; i <= n; i++){
+        for (int i = 1; i <= n; i++) {
             int minn = Integer.MAX_VALUE;
-            for(int j = 1; j * j <= i; j++){
-                minn = Math.min(dp[i - j * j], minn);
+            for (int j = 1; j * j <= i; j++) {
+                minn = Math.min(minn, dp[i - j * j]);
             }
             dp[i] = minn + 1;
         }
